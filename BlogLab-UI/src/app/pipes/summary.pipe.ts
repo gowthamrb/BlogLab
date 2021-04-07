7148be7117a8ba2ce0080d0b86a8530a55c65949
gowthamrb <gowthamrkb@gmail.com>
2021-04-07 08:08:39 +0530

    Changes

--- BlogLab-UI/src/app/pipes/summary.pipe.ts
+++ BlogLab-UI/src/app/pipes/summary.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'summary'
 })
+
 export class SummaryPipe implements PipeTransform {
 
   transform(content: string, characterLimit: number): string {
